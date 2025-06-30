e6da37f1769e8bb8581c00dca60dfae772ae9c49
eduardorittner <eduardorittnerc@gmail.com>
2025-06-30 11:39:26 -0300

    working!

--- mfs.c
+++ mfs.c
@@ -31,15 +31,14 @@ static int myfs_create(
     struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode, bool excl);
 static int myfs_mkdir(
     struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode);
+struct inode* myfs_get_inode(struct super_block* sb, const struct inode* dir, int mode);
 
-/* TODO 2/4: define super_operations structure */
 static const struct super_operations myfs_ops = {
     .statfs = simple_statfs,
     .drop_inode = generic_drop_inode,
 };
 
 static const struct inode_operations myfs_dir_inode_operations = {
-    /* TODO 5/8: Fill dir inode operations structure. */
     .create = myfs_create,
     .lookup = simple_lookup,
     .link = simple_link,
@@ -51,7 +50,6 @@ static const struct inode_operations myfs_dir_inode_operations = {
 };
 
 static const struct file_operations myfs_file_operations = {
-    /* TODO 6/4: Fill file operations structure. */
     .read_iter = generic_file_read_iter,
     .write_iter = generic_file_write_iter,
     .mmap = generic_file_mmap,
@@ -59,20 +57,11 @@ static const struct file_operations myfs_file_operations = {
 };
 
 static const struct inode_operations myfs_file_inode_operations = {
-    /* TODO 6/1: Fill file inode operations structure. */
     .getattr = simple_getattr,
 };
 
 extern int simple_write_end(struct file* file, struct address_space* mapping, loff_t pos,
     unsigned len, unsigned copied, struct page* page, void* fsdata);
-
-static const struct address_space_operations myfs_aops = {
-    /* TODO 6/3: Fill address space operations structure. */
-    //.readpage = simple_readpage,
-    .write_begin = simple_write_begin,
-    // TODO
-    //.write_end = simple_write_end,
-};
 
 struct inode* myfs_get_inode(struct super_block* sb, const struct inode* dir, int mode)
 {
@@ -81,42 +70,23 @@ struct inode* myfs_get_inode(struct super_block* sb, const struct inode* dir, in
     if (!inode)
         return NULL;
 
-    /* TODO 3/3: fill inode structure
-     *     - mode
-     *     - uid
-     *     - gid
-     *     - atime,ctime,mtime
-     *     - ino
-     */
     inode_init_owner(&nop_mnt_idmap, inode, dir, mode);
     inode->__i_atime = inode->__i_mtime = inode->__i_ctime = current_time(inode);
     inode->i_ino = 1;
 
-    /* TODO 5/1: Init i_ino using get_next_ino */
     inode->i_ino = get_next_ino();
 
-    /* TODO 6/1: Initialize address space operations. */
-    inode->i_mapping->a_ops = &myfs_aops;
+    inode->i_mapping->a_ops = &ram_aops;
 
     if (S_ISDIR(mode)) {
-        /* TODO 3/2: set inode operations for dir inodes. */
         inode->i_op = &simple_dir_inode_operations;
         inode->i_fop = &simple_dir_operations;
 
-        /* TODO 5/1: use myfs_dir_inode_operations for inode
-         * operations (i_op).
-         */
         inode->i_op = &myfs_dir_inode_operations;
 
-        /* TODO 3/1: directory inodes start off with i_nlink == 2 (for "." entry).
-         * Directory link count should be incremented (use inc_nlink).
-         */
         inc_nlink(inode);
     }
 
-    /* TODO 6/4: Set file inode and file operations for regular files
-     * (use the S_ISREG macro).
-     */
     if (S_ISREG(mode)) {
         inode->i_op = &myfs_file_inode_operations;
         inode->i_fop = &myfs_file_operations;
@@ -125,10 +95,11 @@ struct inode* myfs_get_inode(struct super_block* sb, const struct inode* dir, in
     return inode;
 }
 
-/* TODO 5/33: Implement myfs_mknod, myfs_create, myfs_mkdir. */
 static int myfs_mknod(
     struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode, dev_t dev)
 {
+    printk(LOG_LEVEL "Creating node: %s\n", dentry->d_iname);
+
     struct inode* inode = myfs_get_inode(dir->i_sb, dir, mode);
 
     if (inode == NULL)
@@ -144,12 +115,15 @@ static int myfs_mknod(
 static int myfs_create(
     struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode, bool excl)
 {
+    printk(LOG_LEVEL "Creating file: %s\n", dentry->d_iname);
+
     return myfs_mknod(idmap, dir, dentry, mode | S_IFREG, 0);
 }
 
 static int myfs_mkdir(
     struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode)
 {
+    printk(LOG_LEVEL "Creating dir: %s\n", dentry->d_iname);
     int ret;
 
     ret = myfs_mknod(idmap, dir, dentry, mode | S_IFDIR, 0);
@@ -166,12 +140,6 @@ static int myfs_fill_super(struct super_block* sb, void* data, int silent)
     struct inode* root_inode;
     struct dentry* root_dentry;
 
-    /* TODO 2/5: fill super_block
-     *   - blocksize, blocksize_bits
-     *   - magic
-     *   - super operations
-     *   - maxbytes
-     */
     sb->s_maxbytes = MAX_LFS_FILESIZE;
     sb->s_blocksize = MYFS_BLOCKSIZE;
     sb->s_blocksize_bits = MYFS_BLOCKSIZE_BITS;
@@ -202,26 +170,34 @@ out_no_root:
 static struct dentry* myfs_mount(
     struct file_system_type* fs_type, int flags, const char* dev_name, void* data)
 {
-    /* TODO 1/1: call superblock mount function */
+    printk(LOG_LEVEL "Mounting myfs filesytem\n");
     return mount_nodev(fs_type, flags, data, myfs_fill_super);
 }
 
-/* TODO 1/6: define file_system_type structure */
+/* We only define this function in order to log, since the actual work is being done by
+ * kill_litter_super */
+static void myfs_umount(struct super_block* sb)
+{
+    printk(LOG_LEVEL "Unmount myfs filesystem\n");
+    kill_litter_super(sb);
+}
+
 static struct file_system_type myfs_fs_type = {
     .owner = THIS_MODULE,
     .name = "myfs",
     .mount = myfs_mount,
-    .kill_sb = kill_litter_super,
+    .kill_sb = myfs_umount,
 };
 
 static int __init myfs_init(void)
 {
     int err;
 
-    /* TODO 1/1: register */
+    printk(LOG_LEVEL "myfs: registering\n");
+
     err = register_filesystem(&myfs_fs_type);
     if (err) {
-        printk(LOG_LEVEL "register_filesystem failed\n");
+        printk(LOG_LEVEL "myfs: register_filesystem failed\n");
         return err;
     }
 
@@ -230,7 +206,8 @@ static int __init myfs_init(void)
 
 static void __exit myfs_exit(void)
 {
-    /* TODO 1/1: unregister */
+    printk(LOG_LEVEL "myfs: unregistering filesystem\n");
+
     unregister_filesystem(&myfs_fs_type);
 }
 
