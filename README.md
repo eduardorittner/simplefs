7a805b7f39a7fc227cc543045a92dc4054572371
eduardorittner <eduardorittnerc@gmail.com>
2025-07-02 21:31:26 -0300

    update readme and some more metrics

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Metrics-fs
+# Metrics-fs (mfs)
 
-Simple in-memory filesystem with global, per-dir and per-file stats.
+Simple in-memory filesystem with global stats.
 
 ## Running
 
@@ -11,3 +11,16 @@ Simple in-memory filesystem with global, per-dir and per-file stats.
 
 And you're good to go!
 
+## Structure
+
+mfs follows from this [filesystem tutorial](https://linux-kernel-labs.github.io/refs/heads/master/labs/filesystems_part1.html), which is itself inspired by ramfs, a simple in-memory filesystem in the linux kernel used mostly for educational purposes. It leverages the kernel's internal functions for most of the file and directory operations.
+
+Mfs collects very basic global metrics for the current mounted instance, such as:
+- Total bytes written
+- Total bytes read
+- Total write operations
+- Total read operations
+- Total files created/deleted
+- Total directories created/deleted
+
+These metrics are available through a special virtual "/metrics" file, which has custom write (no-op) and read operations, and is immutable so cannot be renamed, deleted or moved.
--- mfs.c
+++ mfs.c
@@ -5,6 +5,7 @@
 // --------
 // Includes
 // --------
+#include <linux/atomic.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -46,35 +47,32 @@ static atomic64_t total_dirs_deleted = ATOMIC_INIT(0);
 // Forward declarations for regular files operations
 // -------------------------------------------------
 
-static int mfs_mknod(struct mnt_idmap *idmap, struct inode *dir,
-                     struct dentry *dentry, umode_t mode, dev_t dev);
-static int mfs_create(struct mnt_idmap *idmap, struct inode *dir,
-                      struct dentry *dentry, umode_t mode, bool excl);
-static int mfs_mkdir(struct mnt_idmap *idmap, struct inode *dir,
-                     struct dentry *dentry, umode_t mode);
-static int mfs_rmdir(struct inode *inode, struct dentry *dentry);
-static int mfs_unlink(struct inode *inode, struct dentry *dentry);
-struct inode *mfs_get_inode(struct super_block *sb, const struct inode *dir,
-                            int mode);
+static int mfs_mknod(
+    struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode, dev_t dev);
+static int mfs_create(
+    struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode, bool excl);
+static int mfs_mkdir(
+    struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode);
+static int mfs_rmdir(struct inode* inode, struct dentry* dentry);
+static int mfs_unlink(struct inode* inode, struct dentry* dentry);
+struct inode* mfs_get_inode(struct super_block* sb, const struct inode* dir, int mode);
 
-static ssize_t mfs_write_iter(struct kiocb *kiocb, struct iov_iter *iter);
-static ssize_t mfs_read_iter(struct kiocb *kiocb, struct iov_iter *iter);
+static ssize_t mfs_write_iter(struct kiocb* kiocb, struct iov_iter* iter);
+static ssize_t mfs_read_iter(struct kiocb* kiocb, struct iov_iter* iter);
 
 // This needs to be declared here since it's not publicly exported by the
 // kernel. In the tutorial this wasn't a problem since the filesystem an in-tree
 // module, while we are an out-of-tree module.
-extern int simple_write_end(struct file *file, struct address_space *mapping,
-                            loff_t pos, unsigned len, unsigned copied,
-                            struct page *page, void *fsdata);
+extern int simple_write_end(struct file* file, struct address_space* mapping, loff_t pos,
+    unsigned len, unsigned copied, struct page* page, void* fsdata);
 
 // ------------------------------------
 // Forward declarations for metric file
 // ------------------------------------
 
-static ssize_t mfs_metrics_read(struct file *file, char __user *buf, size_t len,
-                                loff_t *offset);
-static ssize_t mfs_metrics_write(struct file *file, const char __user *buf,
-                                 size_t len, loff_t *offset);
+static ssize_t mfs_metrics_read(struct file* file, char __user* buf, size_t len, loff_t* offset);
+static ssize_t mfs_metrics_write(
+    struct file* file, const char __user* buf, size_t len, loff_t* offset);
 
 // ----------
 // Operations
@@ -107,87 +105,87 @@ static const struct inode_operations mfs_file_inode_operations = {
     .getattr = simple_getattr,
 };
 
-static const struct file_operations mfs_metrics_operations = {
-    .read = mfs_metrics_read, .write = mfs_metrics_write,
-    // TODO do we need to add
-    // .llseek = genereic_file_llseek
-};
+static const struct file_operations mfs_metrics_operations
+    = { .read = mfs_metrics_read, .write = mfs_metrics_write, .llseek = generic_file_llseek };
 
 // -------------------------
 // Operation implementations
 // -------------------------
 
-struct inode *mfs_get_inode(struct super_block *sb, const struct inode *dir,
-                            int mode) {
-  struct inode *inode = new_inode(sb);
+struct inode* mfs_get_inode(struct super_block* sb, const struct inode* dir, int mode)
+{
+    struct inode* inode = new_inode(sb);
 
-  if (!inode)
-    return NULL;
+    if (!inode)
+        return NULL;
 
-  inode_init_owner(&nop_mnt_idmap, inode, dir, mode);
-  inode->__i_atime = inode->__i_mtime = inode->__i_ctime = current_time(inode);
-  inode->i_ino = 1;
+    inode_init_owner(&nop_mnt_idmap, inode, dir, mode);
+    inode->__i_atime = inode->__i_mtime = inode->__i_ctime = current_time(inode);
+    inode->i_ino = 1;
 
-  inode->i_ino = get_next_ino();
+    inode->i_ino = get_next_ino();
 
-  inode->i_mapping->a_ops = &ram_aops;
+    inode->i_mapping->a_ops = &ram_aops;
 
-  if (S_ISDIR(mode)) {
-    inode->i_op = &simple_dir_inode_operations;
-    inode->i_fop = &simple_dir_operations;
+    if (S_ISDIR(mode)) {
+        inode->i_op = &simple_dir_inode_operations;
+        inode->i_fop = &simple_dir_operations;
 
-    inode->i_op = &mfs_dir_inode_operations;
+        inode->i_op = &mfs_dir_inode_operations;
 
-    inc_nlink(inode);
-  }
+        inc_nlink(inode);
+    }
 
-  if (S_ISREG(mode)) {
-    inode->i_op = &mfs_file_inode_operations;
-    inode->i_fop = &mfs_file_operations;
-  }
+    if (S_ISREG(mode)) {
+        inode->i_op = &mfs_file_inode_operations;
+        inode->i_fop = &mfs_file_operations;
+    }
 
-  return inode;
+    return inode;
 }
 
-static int mfs_mknod(struct mnt_idmap *idmap, struct inode *dir,
-                     struct dentry *dentry, umode_t mode, dev_t dev) {
-  printk(LOG_LEVEL "Creating node: %s\n", dentry->d_iname);
+static int mfs_mknod(
+    struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode, dev_t dev)
+{
+    printk(LOG_LEVEL "Creating node: %s\n", dentry->d_iname);
 
-  struct inode *inode = mfs_get_inode(dir->i_sb, dir, mode);
+    struct inode* inode = mfs_get_inode(dir->i_sb, dir, mode);
 
-  if (inode == NULL)
-    return -ENOSPC;
+    if (inode == NULL)
+        return -ENOSPC;
 
-  d_instantiate(dentry, inode);
-  dget(dentry);
-  dir->__i_mtime = dir->__i_ctime = current_time(inode);
+    d_instantiate(dentry, inode);
+    dget(dentry);
+    dir->__i_mtime = dir->__i_ctime = current_time(inode);
 
-  atomic64_inc(&total_inodes_created);
+    atomic64_inc(&total_inodes_created);
 
-  return 0;
+    return 0;
 }
 
-static int mfs_create(struct mnt_idmap *idmap, struct inode *dir,
-                      struct dentry *dentry, umode_t mode, bool excl) {
-  atomic64_inc(&total_files_created);
-  printk(LOG_LEVEL "Creating file: %s\n", dentry->d_iname);
+static int mfs_create(
+    struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode, bool excl)
+{
+    atomic64_inc(&total_files_created);
+    printk(LOG_LEVEL "Creating file: %s\n", dentry->d_iname);
 
-  return mfs_mknod(idmap, dir, dentry, mode | S_IFREG, 0);
+    return mfs_mknod(idmap, dir, dentry, mode | S_IFREG, 0);
 }
 
-static int mfs_mkdir(struct mnt_idmap *idmap, struct inode *dir,
-                     struct dentry *dentry, umode_t mode) {
-  printk(LOG_LEVEL "Creating dir: %s\n", dentry->d_iname);
-  int ret;
+static int mfs_mkdir(
+    struct mnt_idmap* idmap, struct inode* dir, struct dentry* dentry, umode_t mode)
+{
+    printk(LOG_LEVEL "Creating dir: %s\n", dentry->d_iname);
+    int ret;
 
-  ret = mfs_mknod(idmap, dir, dentry, mode | S_IFDIR, 0);
-  if (ret != 0)
-    return ret;
+    ret = mfs_mknod(idmap, dir, dentry, mode | S_IFDIR, 0);
+    if (ret != 0)
+        return ret;
 
-  atomic64_inc(&total_dirs_created);
-  inc_nlink(dir);
+    atomic64_inc(&total_dirs_created);
+    inc_nlink(dir);
 
-  return 0;
+    return 0;
 }
 
 // --------------------------
@@ -195,39 +193,43 @@ static int mfs_mkdir(struct mnt_idmap *idmap, struct inode *dir,
 // --------------------------
 
 // Wrapper that increments write_ops and total_bytes_written on success
-static ssize_t mfs_write_iter(struct kiocb *kiocb, struct iov_iter *iter) {
-  ssize_t bytes_written = generic_file_write_iter(kiocb, iter);
-  if (bytes_written > 0) {
-    atomic64_inc(&total_write_ops);
-    atomic64_add(bytes_written, &total_bytes_written);
-  }
-  return bytes_written;
+static ssize_t mfs_write_iter(struct kiocb* kiocb, struct iov_iter* iter)
+{
+    ssize_t bytes_written = generic_file_write_iter(kiocb, iter);
+    if (bytes_written > 0) {
+        atomic64_inc(&total_write_ops);
+        atomic64_add(bytes_written, &total_bytes_written);
+    }
+    return bytes_written;
 }
 
 // Wrapper that increments read_ops and total_bytes_read on success
-static ssize_t mfs_read_iter(struct kiocb *kiocb, struct iov_iter *iter) {
-  ssize_t bytes_read = generic_file_read_iter(kiocb, iter);
-  if (bytes_read > 0) {
-    atomic64_inc(&total_read_ops);
-    atomic64_add(bytes_read, &total_bytes_read);
-  }
-  return bytes_read;
+static ssize_t mfs_read_iter(struct kiocb* kiocb, struct iov_iter* iter)
+{
+    ssize_t bytes_read = generic_file_read_iter(kiocb, iter);
+    if (bytes_read > 0) {
+        atomic64_inc(&total_read_ops);
+        atomic64_add(bytes_read, &total_bytes_read);
+    }
+    return bytes_read;
 }
 
 // Wrapper that increments total_dirs_deleted on success
-static int mfs_rmdir(struct inode *inode, struct dentry *dentry) {
-  int ret = simple_rmdir(inode, dentry);
-  if (!ret)
-    atomic64_inc(&total_dirs_deleted);
-  return ret;
+static int mfs_rmdir(struct inode* inode, struct dentry* dentry)
+{
+    int ret = simple_rmdir(inode, dentry);
+    if (!ret)
+        atomic64_inc(&total_dirs_deleted);
+    return ret;
 }
 
 // Wrapper that increments total_files_deleted on success
-static int mfs_unlink(struct inode *inode, struct dentry *dentry) {
-  int ret = simple_unlink(inode, dentry);
-  if (!ret)
-    atomic64_inc(&total_files_deleted);
-  return ret;
+static int mfs_unlink(struct inode* inode, struct dentry* dentry)
+{
+    int ret = simple_unlink(inode, dentry);
+    if (!ret)
+        atomic64_inc(&total_files_deleted);
+    return ret;
 }
 
 // -------------------------
@@ -235,111 +237,139 @@ static int mfs_unlink(struct inode *inode, struct dentry *dentry) {
 // -------------------------
 
 // Custom read function which formats current metrics into a buffer
-static ssize_t mfs_metrics_read(struct file *file, char __user *buf, size_t len,
-                                loff_t *offset) {
-  char metrics_buf[1024];
+static ssize_t mfs_metrics_read(struct file* file, char __user* buf, size_t len, loff_t* offset)
+{
+    char metrics_buf[1024];
 
-  // TODO: first read all atomics before formatting
-  // TODO: Display current files and dirs by subtracting created - deleted
-  int metrics_len = scnprintf(
-      metrics_buf, sizeof(metrics_buf),
-      "bytes written: %lld\n"
-      "bytes read: %lld\n"
-      "write operations: %lld\n"
-      "read operations: %lld\n"
-      "files created: %lld\n"
-      "files deleted: %lld\n"
-      "dirs created: %lld\n"
-      "dirs deleted: %lld\n",
-      atomic64_read(&total_bytes_written), atomic64_read(&total_bytes_read),
-      atomic64_read(&total_write_ops), atomic64_read(&total_read_ops),
-      atomic64_read(&total_files_created), atomic64_read(&total_files_deleted),
-      atomic64_read(&total_dirs_created), atomic64_read(&total_dirs_deleted));
+    u64 bytes_written = atomic64_read(&total_bytes_written);
+    u64 bytes_read = atomic64_read(&total_bytes_read);
+    u64 write_ops = atomic64_read(&total_write_ops);
+    u64 read_ops = atomic64_read(&total_read_ops);
+    u64 files_created = atomic64_read(&total_files_created);
+    u64 files_deleted = atomic64_read(&total_files_deleted);
+    u64 files_current = files_created - files_deleted;
+    u64 dirs_created = atomic64_read(&total_dirs_created);
+    u64 dirs_deleted = atomic64_read(&total_dirs_deleted);
+    u64 dirs_current = dirs_created - dirs_deleted;
 
-  // Copies the kernel space memory in metrics_buf correctly to the user space
-  // memory in buf
-  return simple_read_from_buffer(buf, len, offset, metrics_buf, metrics_len);
+    int metrics_len = scnprintf(metrics_buf, sizeof(metrics_buf),
+        "bytes written: %lld\n"
+        "bytes read: %lld\n"
+        "write operations: %lld\n"
+        "read operations: %lld\n"
+        "files created: %lld\n"
+        "files deleted: %lld\n"
+        "current number of files: %lld\n"
+        "dirs created: %lld\n"
+        "dirs deleted: %lld\n"
+        "current number of dirs: %lld\n",
+        bytes_written, bytes_read, write_ops, read_ops, files_created, files_deleted, files_current,
+        dirs_created, dirs_deleted, dirs_current);
+
+    // Copies the kernel space memory in metrics_buf correctly to the user space
+    // memory in buf
+    return simple_read_from_buffer(buf, len, offset, metrics_buf, metrics_len);
 }
 
 // No-op write function which returns "Permission denied" on every write
-static ssize_t mfs_metrics_write(struct file *file, const char __user *buf,
-                                 size_t len, loff_t *offset) {
-  return -EACCES;
+static ssize_t mfs_metrics_write(
+    struct file* file, const char __user* buf, size_t len, loff_t* offset)
+{
+    return -EACCES;
 }
 
 // -------------------
 // Filesystem routines
 // -------------------
 
-static int mfs_fill_super(struct super_block *sb, void *data, int silent) {
-  struct inode *root_inode;
-  struct dentry *root_dentry;
-  struct inode *metrics_inode;
-  struct dentry *metrics_dentry;
+static int mfs_fill_super(struct super_block* sb, void* data, int silent)
+{
+    struct inode* root_inode;
+    struct dentry* root_dentry;
+    struct inode* metrics_inode;
+    struct dentry* metrics_dentry;
 
-  // Mfs properties
-  sb->s_maxbytes = MAX_LFS_FILESIZE;
-  sb->s_blocksize = mfs_BLOCKSIZE;
-  sb->s_blocksize_bits = mfs_BLOCKSIZE_BITS;
-  sb->s_magic = mfs_MAGIC;
-  sb->s_op = &mfs_ops;
+    // Mfs properties
+    sb->s_maxbytes = MAX_LFS_FILESIZE;
+    sb->s_blocksize = mfs_BLOCKSIZE;
+    sb->s_blocksize_bits = mfs_BLOCKSIZE_BITS;
+    sb->s_magic = mfs_MAGIC;
+    sb->s_op = &mfs_ops;
 
-  /* mode = directory & access rights (755) */
-  root_inode = mfs_get_inode(
-      sb, NULL, S_IFDIR | S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+    /* mode = directory & access rights (755) */
+    root_inode = mfs_get_inode(sb, NULL, S_IFDIR | S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
 
-  printk(LOG_LEVEL "root inode has %d link(s)\n", root_inode->i_nlink);
+    printk(LOG_LEVEL "root inode has %d link(s)\n", root_inode->i_nlink);
 
-  if (!root_inode)
-    return -ENOMEM;
+    if (!root_inode)
+        return -ENOMEM;
 
-  root_dentry = d_make_root(root_inode);
-  if (!root_dentry)
-    goto out_root_inode;
-  sb->s_root = root_dentry;
+    root_dentry = d_make_root(root_inode);
+    if (!root_dentry)
+        goto out_root_inode;
+    sb->s_root = root_dentry;
 
-  // Create "metrics" file
+    // Create "metrics" file
 
-  // Allocate dentry
-  metrics_dentry = d_alloc_name(root_dentry, "metrics");
-  if (!metrics_dentry)
-    goto out_root;
+    // Allocate dentry
+    metrics_dentry = d_alloc_name(root_dentry, "metrics");
+    if (!metrics_dentry)
+        goto out_root;
 
-  // Allocate inode
-  metrics_inode = mfs_get_inode(sb, root_inode, S_IFREG | 0444);
-  if (!metrics_inode) {
-    dput(metrics_dentry);
-    goto out_root;
-  }
+    // Allocate inode
+    metrics_inode = mfs_get_inode(sb, root_inode, S_IFREG | 0444);
+    if (!metrics_inode) {
+        dput(metrics_dentry);
+        goto out_root;
+    }
 
-  // Set custom operations
-  metrics_inode->i_fop = &mfs_metrics_operations;
-  // Link dentry and inode
-  d_add(metrics_dentry, metrics_inode);
+    // Set custom read/write operations
+    metrics_inode->i_fop = &mfs_metrics_operations;
+    // Set metric inode's immutable flag so it can't be renamed, moved or deleted
+    metrics_inode->i_flags |= S_IMMUTABLE;
+    // Link dentry and inode
+    d_add(metrics_dentry, metrics_inode);
 
-  return 0;
+    return 0;
 
-  // Deallocates root inode and dentry
+    // Deallocates root inode and dentry
 out_root:
-  dput(root_dentry);
+    dput(root_dentry);
 
-  // Deallocates root inode
+    // Deallocates root inode
 out_root_inode:
-  iput(root_inode);
-  return -ENOMEM;
+    iput(root_inode);
+    return -ENOMEM;
 }
 
-static struct dentry *mfs_mount(struct file_system_type *fs_type, int flags,
-                                const char *dev_name, void *data) {
-  printk(LOG_LEVEL "Mounting mfs filesytem\n");
-  return mount_nodev(fs_type, flags, data, mfs_fill_super);
+static struct dentry* mfs_mount(
+    struct file_system_type* fs_type, int flags, const char* dev_name, void* data)
+{
+    printk(LOG_LEVEL "Mounting mfs filesytem\n");
+    return mount_nodev(fs_type, flags, data, mfs_fill_super);
+}
+
+static void zero_counters(void)
+{
+    atomic64_set(&total_bytes_written, 0);
+    atomic64_set(&total_bytes_read, 0);
+    atomic64_set(&total_read_ops, 0);
+    atomic64_set(&total_write_ops, 0);
+    atomic64_set(&total_inodes_created, 0);
+    atomic64_set(&total_files_created, 0);
+    atomic64_set(&total_files_deleted, 0);
+    atomic64_set(&total_dirs_created, 0);
+    atomic64_set(&total_dirs_deleted, 0);
 }
 
 // We only define this function in order to log the umount operation, since
 // the actual work is being done by kill_litter_super
-static void mfs_umount(struct super_block *sb) {
-  printk(LOG_LEVEL "Unmount mfs filesystem\n");
-  kill_litter_super(sb);
+static void mfs_umount(struct super_block* sb)
+{
+    printk(LOG_LEVEL "Unmount mfs filesystem\n");
+
+    zero_counters();
+    kill_litter_super(sb);
 }
 
 static struct file_system_type mfs_fs_type = {
@@ -349,24 +379,26 @@ static struct file_system_type mfs_fs_type = {
     .kill_sb = mfs_umount,
 };
 
-static int __init mfs_init(void) {
-  int err;
+static int __init mfs_init(void)
+{
+    int err;
 
-  printk(LOG_LEVEL "mfs: registering\n");
+    printk(LOG_LEVEL "mfs: registering\n");
 
-  err = register_filesystem(&mfs_fs_type);
-  if (err) {
-    printk(LOG_LEVEL "mfs: register_filesystem failed\n");
-    return err;
-  }
+    err = register_filesystem(&mfs_fs_type);
+    if (err) {
+        printk(LOG_LEVEL "mfs: register_filesystem failed\n");
+        return err;
+    }
 
-  return 0;
+    return 0;
 }
 
-static void __exit mfs_exit(void) {
-  printk(LOG_LEVEL "mfs: unregistering filesystem\n");
+static void __exit mfs_exit(void)
+{
+    printk(LOG_LEVEL "mfs: unregistering filesystem\n");
 
-  unregister_filesystem(&mfs_fs_type);
+    unregister_filesystem(&mfs_fs_type);
 }
 
 // Register init and exit routines
